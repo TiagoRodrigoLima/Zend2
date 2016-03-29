8542d104f04c7b03d74608785d176d46146bbcc3
Tiago Rodrigo de Lima <tiagorodrigodelima@hotmail.com>
2016-03-29 10:56:59 -0300

    #004 - Conclusão App Album

--- module/Album/src/Album/Model/Album.php
+++ module/Album/src/Album/Model/Album.php
@@ -1,6 +1,6 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -8,16 +8,84 @@
 
 namespace Album\Model;
 
- class Album
- {
-     public $id;
-     public $artist;
-     public $title;
+use Zend\InputFilter\InputFilter;
+use Zend\InputFilter\InputFilterAwareInterface;
+use Zend\InputFilter\InputFilterInterface;
 
-     public function exchangeArray($data)
-     {
-         $this->id     = (!empty($data['id'])) ? $data['id'] : null;
-         $this->artist = (!empty($data['artist'])) ? $data['artist'] : null;
-         $this->title  = (!empty($data['title'])) ? $data['title'] : null;
-     }
- }
+class Album {
+
+    public $id;
+    public $artist;
+    public $title;
+
+    public function exchangeArray($data) {
+        $this->id = (!empty($data['id'])) ? $data['id'] : null;
+        $this->artist = (!empty($data['artist'])) ? $data['artist'] : null;
+        $this->title = (!empty($data['title'])) ? $data['title'] : null;
+    }
+    
+    public function getArrayCopy() {
+        return get_object_vars($this);
+    }
+
+    public function setInputFilter(InputFilterInterface $inputFilter) {
+        throw new \Exception("Not used");
+    }
+
+    public function getInputFilter() {
+        if (!$this->inputFilter) {
+            $inputFilter = new InputFilter();
+
+            $inputFilter->add(array(
+                'name' => 'id',
+                'required' => true,
+                'filters' => array(
+                    array('name' => 'Int'),
+                ),
+            ));
+
+            $inputFilter->add(array(
+                'name' => 'artist',
+                'required' => true,
+                'filters' => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name' => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min' => 1,
+                            'max' => 100,
+                        ),
+                    ),
+                ),
+            ));
+
+            $inputFilter->add(array(
+                'name' => 'title',
+                'required' => true,
+                'filters' => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name' => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min' => 1,
+                            'max' => 100,
+                        ),
+                    ),
+                ),
+            ));
+
+            $this->inputFilter = $inputFilter;
+        }
+
+        return $this->inputFilter;
+    }
+
+}
--- module/Album/src/Album/Model/AlbumTable.php
+++ module/Album/src/Album/Model/AlbumTable.php
@@ -1,6 +1,6 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -8,55 +8,51 @@
 
 namespace Album\Model;
 
- use Zend\Db\TableGateway\TableGateway;
+use Zend\Db\TableGateway\TableGateway;
 
- class AlbumTable
- {
-     protected $tableGateway;
+class AlbumTable {
 
-     public function __construct(TableGateway $tableGateway)
-     {
-         $this->tableGateway = $tableGateway;
-     }
+    protected $tableGateway;
 
-     public function fetchAll()
-     {
-         $resultSet = $this->tableGateway->select();
-         return $resultSet;
-     }
+    public function __construct(TableGateway $tableGateway) {
+        $this->tableGateway = $tableGateway;
+    }
 
-     public function getAlbum($id)
-     {
-         $id  = (int) $id;
-         $rowset = $this->tableGateway->select(array('id' => $id));
-         $row = $rowset->current();
-         if (!$row) {
-             throw new \Exception("Could not find row $id");
-         }
-         return $row;
-     }
+    public function fetchAll() {
+        $resultSet = $this->tableGateway->select();
+        return $resultSet;
+    }
 
-     public function saveAlbum(Album $album)
-     {
-         $data = array(
-             'artist' => $album->artist,
-             'title'  => $album->title,
-         );
+    public function getAlbum($id) {
+        $id = (int) $id;
+        $rowset = $this->tableGateway->select(array('id' => $id));
+        $row = $rowset->current();
+        if (!$row) {
+            throw new \Exception("Could not find row $id");
+        }
+        return $row;
+    }
 
-         $id = (int) $album->id;
-         if ($id == 0) {
-             $this->tableGateway->insert($data);
-         } else {
-             if ($this->getAlbum($id)) {
-                 $this->tableGateway->update($data, array('id' => $id));
-             } else {
-                 throw new \Exception('Album id does not exist');
-             }
-         }
-     }
+    public function saveAlbum(Album $album) {
+        $data = array(
+            'artist' => $album->artist,
+            'title' => $album->title,
+        );
 
-     public function deleteAlbum($id)
-     {
-         $this->tableGateway->delete(array('id' => (int) $id));
-     }
- }
+        $id = (int) $album->id;
+        if ($id == 0) {
+            $this->tableGateway->insert($data);
+        } else {
+            if ($this->getAlbum($id)) {
+                $this->tableGateway->update($data, array('id' => $id));
+            } else {
+                throw new \Exception('Album id does not exist');
+            }
+        }
+    }
+
+    public function deleteAlbum($id) {
+        $this->tableGateway->delete(array('id' => (int) $id));
+    }
+
+}
